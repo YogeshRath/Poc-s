773c544c5e2d74a3195b1544b07f114941541cdf
Rathor Yogesh <rathoreyogesh06@gmail.com>
2019-12-06 11:52:33 +0530

    SSO Sample Application For demo

---
+++ src/main/java/com/yrath/demo/SamlApp/SamlAppApplication.java
@@ -0,0 +1,21 @@
+package com.yrath.demo.SamlApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class SamlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlAppApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String baseApp() {
+		return "index";
+	}
+
+}
---
+++ src/test/java/com/yrath/demo/SamlApp/SamlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yrath.demo.SamlApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
